53ea7da80aa0f9d561c3f4d01af4f1194dbbdd13
SK21-code <shraddhadevendrakamble@gmail.com>
2026-03-17 14:52:02 +0530

    Add files via upload

---
+++ relatoin.java
@@ -0,0 +1,14 @@
+public class relatoin {
+    
+
+    public static void main(String[] args) {
+
+        int a=10,b=20;      
+        
+        System.out.println(a<b || b>30);
+        System.out.println(a>b || b<30);
+        System.out.println(!true);
+        System.out.println(a<b && b>30);
+        System.out.println(a>b && b<30);
+    }
+}        
