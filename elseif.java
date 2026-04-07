a58e27751508516992a4711d206b38a051135fb8
SK21-code <shraddhadevendrakamble@gmail.com>
2026-04-07 09:32:20 +0530

    visual studio code

---
+++ elseif.java
@@ -0,0 +1,23 @@
+public class elseif {
+    public static void main(String[] args) {
+        int num =100;
+
+        if(num>=90 && num<80){
+            System.out.println("Grade A");
+        }else if(num>=80 && num>70){
+            System.out.println("Grade B");
+        }else if(num>=70 && num>60){
+            System.out.println("Grade C");
+        }else if (num>=60 && num>45){
+            System.out.println("Grade D");
+        }   else {
+            System.out.println("Fail");
+
+        }
+             
+
+        }
+        
+    }
+
+
