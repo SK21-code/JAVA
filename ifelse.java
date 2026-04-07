0d1463fb547d21ad58d673adb04edb796de6f4d2
SK21-code <shraddhadevendrakamble@gmail.com>
2026-04-07 09:27:16 +0530

    Add files via upload

--- ifelse.java
+++ ifelse.java
@@ -4,16 +4,16 @@ public class ifelse {
     
 
     public static void main(String[] args) {
-        int num ;
+
         Scanner obj=new Scanner(System.in);
-       System.out.println("Enter num");
-        num = obj.nextInt();
+           int x = obj.nextInt();
+          if(x %2 == 0){
+            System.out.println("Even");
+            else {
+                System.out.println("Odd");
+            }
 
-       if(num%2==0){
-        System.out.println("Its even");
-
-       }else{
-        System.out.println("it is odd");
-       }
+            }
+          }
     }
-}
+
